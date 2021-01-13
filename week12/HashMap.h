358adf8a5c61ec48aee86b63f8c7402b67191233
Damian <dam-bb@hotmail>
2021-01-13 11:55:44 +0200

    Comment out the c++20 concepts implementation

--- week12/HashMap.h
+++ week12/HashMap.h
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <functional>
 #include <vector>
-#include <string>
-#include <type_traits>
-#include <concepts>
+#include <utility>
+// #include <type_traits> for concepts
+// #include <concepts> // C++20 only
 
 // template<typename Key, typename Value>
 // struct Pair
@@ -12,46 +12,42 @@
 //     Value value;
 // };
 
-template<typename Key, typename Value>
-struct Node
-{
-    // Pair<Key, Value> pair;
-    Key key;
-    Value value;
-    Node *next;
-};
-
-
+/* // for concept
 class Hashable {
 public:
     virtual size_t hash() const = 0;
 };
+*/
 
-
+/* // C++20 only
 template<typename T>
 concept is_hashable = std::derived_from<T, Hashable>;
+*/
 
-
-template<is_hashable Key, typename Value>
+template<typename /* is_hashable c++20 only */  Key, typename Value>
 class HashMap
 {
 private:
-    std::vector<Node<Key, Value>*> table;
+    struct Node
+    {
+        Key key;
+        Value value;
+        Node *next;
+    };
+
+    std::vector<Node* > table;
     std::function<size_t(const Key&)> hashFunction;
 
+    
 public:
-    HashMap(size_t capacity = 1024):table(capacity){}
     HashMap(std::function<size_t(const Key&)> _hashFunction, size_t capacity = 1024):table(capacity, nullptr) {
         this->hashFunction = _hashFunction;
     }
 
-
-    // void add(...) {
-    //     table[....hash() % table.size()] = ...
-    // }
+    /*
+    void add(...) {
+        
+    }
+     */
 };
-
-
-
-
 
